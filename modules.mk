9c901f857684593bd4b7ce290be1e4ee2e85e52e
electimon <electimon@gmail.com>
2023-02-05 01:46:35 +0800

    [DNM] sphynx: Add sphynx specific machine drivers to modules list
    test: verify /proc/asound/cards lists rt5677 machine driver
    DNM: to be tested
    Signed-off-by: electimon <electimon@gmail.com>

--- modules.mk
+++ modules.mk
@@ -34,6 +34,12 @@ BOARD_VENDOR_KERNEL_MODULES_LOAD += \
 BOARD_VENDOR_KERNEL_MODULES_LOAD += \
     bluedroid_pm
 
+# Sphynx specific audio drivers
+BOARD_VENDOR_KERNEL_MODULES_LOAD += \
+    snd_soc_max98357a \
+    snd_soc_rt5677 \
+    snd_soc_nau8825
+
 # Tegra hdmi audio
 BOARD_VENDOR_KERNEL_MODULES_LOAD += \
     snd_hda_tegra
